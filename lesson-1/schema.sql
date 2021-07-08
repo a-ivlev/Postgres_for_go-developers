bc05ebab43e6cb8454997999ad1197212d1973a0
Aleksandr <a.y.ivlev@gmail.com>
2021-07-08 15:39:20 +0300

    fixed datetime to timestamp

--- lesson-1/schema.sql
+++ lesson-1/schema.sql
@@ -24,6 +24,7 @@ CREATE TABLE rent_price (
     id serial not null unique,
     -- Наименование варианта аренды. Например по часам, или по дням и т.д.
     name varchar(255) not null unique,
+    -- Цена аренды за единицу (час, день, неделя и т.д.).
     price int not null
 );
 
@@ -36,7 +37,7 @@ CREATE TABLE item (
     -- Из связанной таблицы (rent_price) выбираем вариант аренды.
     rent_price_id int not null,
     -- В это поле записывается информация о том когда (дата и время) должна закончиться аренда.
-    expires_at datetime
+    expires_at timestamp,
 
     CONSTRAINT item_rent_price_id FOREIGN KEY (rent_price_id) REFERENCES rent_price (id) ON DELETE cascade
 );
@@ -48,21 +49,22 @@ CREATE TABLE rent_list (
     client_id int not null,
     -- Из связанной таблицы (item) выбираем вещь которую клиент берёт в аренду.
     item_id int not null,
-    -- Из связанной таблицы (rent_price) выбираем подходящий вариант аренды (с оплатой по часам, по дням и т.д.)
+    -- Из связанной таблицы (item), а она в свою очередь из связанной таблицы rent_price устанавливаем подходящий вариант аренды (с оплатой по часам, по дням и т.д.)
     rent_price int not null,
     -- В этом поле устанавливаем срок на который оформляется аренда. 
     duration int not null,
     -- В это поле расчитывается и записывается информация о стоимости аренды.
     rental_amount int not null,
     -- В это поле записывается дата и время начала аренды.
-    start_at datetime not null,
+    start_at timestamp not null,
     -- В это поле записывается информация о фактической дате и времени завершения аренды.
     -- И мы можем его сравнить с датой и временем из таблицы (item поле expires_at) когда должна была завершиться аренда.
-    end_at datetime,
+    end_at timestamp,
     -- В это поле рассчитывается и заноситься информация о необходимости совершения доплаты если время аренды было превышено.
-    surcharge int
+    surcharge int,
 
     CONSTRAINT rent_list_client_id FOREIGN KEY (client_id) REFERENCES client (id),
     CONSTRAINT rent_list_item_id FOREIGN KEY (item_id) REFERENCES item (id),
+    -- Эту связь не удалось добавить.
     CONSTRAINT rent_list_rent_price_item FOREIGN KEY (rent_price) REFERENCES item ("rent_price_id", "price")
 );
